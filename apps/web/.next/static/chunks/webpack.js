f526bb65d88f28f22c932d8ade58f54335704ca4
Christian Matkovic <cmaatko@gmail.com>
2025-10-25 01:24:40 +0200

    Update healing dashboard deployment

--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e40ecab7fac4b425"; }
+/******/ 		__webpack_require__.h = function() { return "10faadab30f75082"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
