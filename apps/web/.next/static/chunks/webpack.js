b5ee3d844b767e2fda39d21ab8ac426f40701104
Christian Matkovic <cmaatko@gmail.com>
2025-10-25 01:25:37 +0200

    Fix TypeScript error in notifications API

--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10faadab30f75082"; }
+/******/ 		__webpack_require__.h = function() { return "d316dd900aeafb27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
